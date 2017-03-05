1315ca710d695b6be5cf2a4b65651a1856308c45
Ankit Thakur <ankitthakur94@gmail.com>
2017-03-06 00:16:17 +0530

    Adding max_elem_in_right_sub_array

---
+++ Random_Algorithms/max_element_in_right_sub_array.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+#define SIZE 11
+int result_arr [SIZE];
+
+using namespace std;
+
+void swap (int& left, int& right)
+{
+	int temp = left;
+	left = right;
+	right = temp;
+}
+
+void  GetMaxInRightSubArrayForEachElem (int arr[] , int left_idx, int right_idx)
+{
+	int max_elem = -1;
+	for (int i= right_idx; i >= left_idx; i--)
+	{
+		cout << " Processing i = " << i << endl;
+
+		if (arr[i] >= max_elem)
+		{
+			max_elem = arr[i];
+			result_arr[i] = -1;
+			continue;
+		}
+		else
+		{
+			result_arr[i] = max_elem;
+			continue;
+		}
+
+		if (i == right_idx)
+		{
+			result_arr[i] = -1;
+			continue;
+		}
+	}	
+
+}
+
+int main ()
+{
+
+	int arr[SIZE];
+	srand (time (NULL));
+
+	for (int i=0; i < SIZE; i++)
+	{
+		arr[i] = rand () % 20 + 1;
+		cout << arr[i] << " , " ;
+	}
+	cout << endl;
+
+	GetMaxInRightSubArrayForEachElem (arr, 0, (SIZE-1) );
+	
+	for (int i=0; i < SIZE; i++)
+	{
+		cout << result_arr[i] << " , " ;
+	}
+
+
+	return 0;
+}
